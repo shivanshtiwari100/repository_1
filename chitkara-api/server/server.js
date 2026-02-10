46016dbbf0332338d0d270f23945fb19d027188a
Shivansh Tiwari <shivanshtiwari100@gmail.com>
2026-02-10 13:08:52 +0530

    Fix: Use dynamic PORT from environment variable for Railway deployment

--- chitkara-api/server/server.js
+++ chitkara-api/server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { fibonacci, getPrimes, getLCM, getHCF } = require('./functions');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
